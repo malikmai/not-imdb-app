affe2ad3e60c6cc5ad7bc62a43177688dc46e07f
returnitems <diwan.umang@gmail.com>
2024-05-23 20:41:40 -0400

    built index fetch service. working on rendering list of movies

--- front-end/not-imdb/src/App.jsx
+++ front-end/not-imdb/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import * as movieService from './services/movieService.js'
+import { useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [movieList, setMovieList] = useState([]);
 
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const data = await movieService.index();
+      setMovieList(data)
+    }
+    fetchMovies();
+  }, []);
+
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <> 
+    <h1>Welcome to the Not-International-Movie-Database!</h1>
     </>
   )
-}
+};
 
-export default App
+export default App;
